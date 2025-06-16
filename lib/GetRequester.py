76a9f6bc23192c2b12904841b54acfab45925505
dennissammy77 <dennissammy77@gmail.com>
2025-06-16 10:04:13 +0300

    data lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,17 @@
 import requests
 import json
 
-class GetRequester:
 
+class GetRequester:
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        response.raise_for_status()
+        return response.content  # returns bytes
 
     def load_json(self):
-        pass
+        response = requests.get(self.url)
+        response.raise_for_status()
+        return response.json()
